f77dbea271567b90a98eb5602a786c4a16db2900
mathiasTonini <43469697+mathiasTonini@users.noreply.github.com>
2024-04-22 13:36:23 +0200

    Add working model

--- main.py
+++ main.py
@@ -25,6 +25,23 @@ def pre_process_ground_truth(image):
                 image.putpixel((x, y), (255,192,203))  # Mettre à noir le pixel
     return image
 
+def post_process_prediction(predictions):
+    _,height, width, classes = predictions.shape
+    rgb_image = np.zeros((height, width, 3), dtype=np.uint8)
+    
+    # Parcourir chaque pixel de l'image étiquetée
+    for prediction in predictions:
+        for x in range(height):
+            for y in range(width):
+                label = np.argmax(prediction[x, y])
+                if label == 4:
+                    rgb_value = (255,255,255)
+                else:    
+                    rgb_value = np.array([i for i in  constant.MAPPING_LABEL if constant.MAPPING_LABEL[i]==label])
+                rgb_image[x][y] = rgb_value
+
+        return rgb_image
+
 def pre_process_all_ground_truth_images():
      for file in os.listdir(constant.ORG):
         ground_truth_file = constant.GROUND_TRUTH + file.replace(".jpg",".gif")
@@ -32,6 +49,14 @@ def pre_process_all_ground_truth_images():
         pre_process_ground_truth(ground_truth_image).save(constant.PROCESS_GROUND_TRUTH+"/"+file.replace(".jpg",".gif"))
 
 if __name__ == "__main__":
-    #pre_process_all_ground_truth_images() ### A commenter si l'on a pas besoin de pre-process les ground truth: On met juste en noir tous les pixels dans les gorund truth que l'on a pas beson de détecter. Cela permet de ne pas entrainter notre model sur des points non nécessaire
-    trained_model, test_loss, test_acc = model.run_model()
-    print(test_loss, test_acc)
+   #pre_process_all_ground_truth_images() ### A commenter si l'on a pas besoin de pre-process les ground truth: On met juste en noir tous les pixels dans les gorund truth que l'on a pas beson de détecter. Cela permet de ne pas entrainter notre model sur des points non nécessaire
+   #trained_model, test_loss, test_acc = model.run_model()
+   #print(test_loss, test_acc)
+   #model.save_my_model(trained_model)
+   my_trained_model = model.load_model('model_test.keras')
+   input = model.load_and_preprocess_images_input(constant.ORG,80,81)
+   prediction = my_trained_model.predict(input)
+   predicted_rgb_image = post_process_prediction(prediction)
+   image = Image.fromarray(np.uint8(predicted_rgb_image))
+   image.save("test.jpg")
+
--- model.py
+++ model.py
@@ -3,12 +3,15 @@ from PIL import Image
 import os
 import tensorflow as tf
 from tensorflow.keras import layers, models
+from tensorflow.keras.models import save_model
+from tensorflow.keras.models import load_model
+
 import constant
 
 def load_and_preprocess_images_input(image_path,start, end):
     images = []
     for index,file  in enumerate(os.listdir(image_path)):
-        if index <= start:
+        if index < start:
             continue
         elif index >= end:
             break
@@ -18,52 +21,60 @@ def load_and_preprocess_images_input(image_path,start, end):
 
 def load_and_preprocess_images_output(image_path,start, end):
     images = load_and_preprocess_images_input(image_path,start, end)
-    pixels = []
+   
+    
+    result = []
+    
     for image in images:
-        width = image.shape[0]
-        height = image.shape[1]
-        tmp = []
+        width, height, _ = image.shape
+        tmp = np.zeros((width, height), dtype=np.int32)
         for x in range(width):
             for y in range(height):
-                tmp.append(image[x][y]) 
-        pixels.append(tmp)
-    return pixels
+                for label, value in constant.MAPPING_LABEL.items():
+                    if np.array_equal(image[x, y], label):
+                        tmp[x,y] = value
+                        break
+        result.append(tmp)
+
+    return np.array(result)
 
 def run_model():
 
     image_height = 960
     image_width = 640
     num_channels = 3
-    num_classes = 4
-    num_epochs = 5
-    train_images = load_and_preprocess_images_input(constant.ORG,0,30)
-    train_labels = load_and_preprocess_images_output(constant.PROCESS_GROUND_TRUTH,0,30) 
-    test_images = load_and_preprocess_images_input(constant.ORG, 31,40)
-    test_labels = load_and_preprocess_images_output(constant.PROCESS_GROUND_TRUTH,31,40) 
+    num_classes = 3
+    num_epochs = 50
+    train_images = load_and_preprocess_images_input(constant.ORG,0,50)
+    train_labels = load_and_preprocess_images_output(constant.PROCESS_GROUND_TRUTH,0,50) 
+    test_images = load_and_preprocess_images_input(constant.ORG,70,72)
+    test_labels = load_and_preprocess_images_output(constant.PROCESS_GROUND_TRUTH,70,72) 
 
 
 # Créer un modèle séquentiel
-    model = models.Sequential([
-        layers.Conv2D(16, (3, 3), activation='relu', input_shape=(image_height, image_width, num_channels)),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(32, (3, 3), activation='relu'),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(num_classes, (1, 1), activation='softmax')  # Couche de sortie pour la segmentation
-    ])
-
+    model = models.Sequential()
+    model.add(layers.Dense(16, input_shape=(image_height,image_width,num_channels)))
+    model.add(layers.Dense(32)),
+    model.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu')),
+    model.add(layers.Dense(32)),
+    model.add(layers.Conv2D(16, (2, 2), padding='same', activation='relu')),
+    model.add(layers.Dense(4))
 
     model.compile(optimizer='adam',
                 loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
 
   
-
+    model.summary()
     model.fit(train_images,train_labels, epochs=num_epochs, validation_data=(test_images, test_labels))
     test_loss, test_acc = model.evaluate(test_images, test_labels)
     return model,test_loss, test_acc
 
+def save_my_model(model):
+    save_model(model, 'model_test.keras')
 
-
+def load_my_model(model):
+    return load_model(model)
 
 
 
