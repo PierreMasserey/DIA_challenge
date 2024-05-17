19e225c5bea7a2b6c4f7062392b446efb8d92bc1
PierreMasserey <119418515+PierreMasserey@users.noreply.github.com>
2024-05-17 11:11:54 +0200

    Update compute_iou.py
    commented unnecessary line

--- compute_iou.py
+++ compute_iou.py
@@ -10,6 +10,7 @@ def create_polygons(csv):
 
     df = pd.read_csv(os.path.realpath(csv))
 
+    #pour ajouter les coordonnées à la liste qui deviendra le csv
     for idx, row in df.iterrows():
         color = str(row['Color'])  # Convertir en chaîne de caractères
         if current_id is None:
@@ -19,7 +20,7 @@ def create_polygons(csv):
                 polygons[current_id] = []
             if len(current_points) >= 3:
                 polygons[current_id].append(current_points)
-            current_points = []  # Réinitialiser la liste des points
+            current_points = []  
             current_id = color
         current_points.append((row['X'], row['Y']))  # Ajouter les coordonnées à la liste
 
@@ -39,31 +40,32 @@ def calculate_iou(poly1, poly2):
 def match_polygons(polygons1, polygons2):
     matched_polygons = {}
 
-    # Créer des groupes de polygones pour chaque couleur dans chaque jeu de données
+    
     groups1 = {}
     groups2 = {}
 
     for id1, points_list1 in polygons1.items():
-        color = id1.split('_')[0] if id1 is not None else None  # Extraire la couleur de l'ID
+        color = id1.split('_')[0] if id1 is not None else None
         if color not in groups1:
             groups1[color] = []
         groups1[color].extend([(id1, points) for points in points_list1])
 
     for id2, points_list2 in polygons2.items():
-        color = id2.split('_')[0] if id2 is not None else None  # Extraire la couleur de l'ID
+        color = id2.split('_')[0] if id2 is not None else None
         if color not in groups2:
             groups2[color] = []
         groups2[color].extend([(id2, points) for points in points_list2])
 
-    # Comparer les polygones de chaque couleur
+    
     for color, points_list2 in groups2.items():
         if color not in groups1:
             print(f"Aucun polygone correspondant trouvé pour la couleur {color} dans le premier fichier.")
             continue
 
-        matched_polygons[color] = []  # Initialiser la liste des correspondances pour cette couleur
+        matched_polygons[color] = []
 
-        matched_ids = set()  # Garder une trace des IDs du fichier 1 qui ont des correspondances dans le fichier 2
+        #les ids qui sont correspondant sont dans cette liste
+        matched_ids = set()  
 
         for id2, points2 in points_list2:
             max_iou = -1
@@ -74,7 +76,7 @@ def match_polygons(polygons1, polygons2):
                 if iou > max_iou:
                     max_iou = iou
                     matching_polygon = points1
-                    matching_id = id1  # Garder l'ID du premier fichier pour le polygone correspondant
+                    matching_id = id1  
             if matching_id not in matched_ids:
                 matched_ids.add(matching_id)
                 matched_polygons[color].append({'id': matching_id, 'polygon': matching_polygon})
@@ -101,22 +103,16 @@ def compute_iou(csv1, csv2):
 
 
 
-    # Compter le nombre d'IDs dans chaque fichier
+    #nbr d'ID
     num_ids_csv1 = len(polygons1)
     num_ids_csv2 = len(polygons2)
 
-
-
-    # Compter le nombre d'IDs dans le fichier 2 ayant un ID correspondant dans le fichier 1
     num_matched_ids = sum(len(matches) for matches in matched_polygons.values())
 
-    # Trouver les IDs dans le fichier 1 sans correspondance dans le fichier 2
     unmatched_ids_csv1 = [id for id in polygons1 if id not in matched_polygons]
 
-    # Trouver les IDs dans le fichier 2 sans correspondance dans le fichier 1
     unmatched_ids_csv2 = [id for id in polygons2 if id.split('_')[0] not in matched_polygons]
 
-    # Ajouter les IDs sans correspondance dans le fichier 2 uniquement si le fichier 1 est différent du fichier 2
     if csv1 != csv2:
         num_ids_csv2 += len(unmatched_ids_csv1)
 
@@ -126,7 +122,7 @@ def compute_iou(csv1, csv2):
     print(f"Nombre de zones false positive : {num_ids_csv2 - (num_matched_ids+1)}")
     print(f"Nombre de zones false negative : {len(unmatched_ids_csv2)}")
 
-    # Afficher les correspondances
+
     iou_results = []
     total_iou = 0
 
@@ -157,7 +153,6 @@ def compute_iou(csv1, csv2):
         print("Aucun IoU calculé car aucune correspondance trouvée.")
 
 
-    # Afficher les IDs sans correspondance dans le fichier 2
     for id in unmatched_ids_csv2:
         print(f"Aucun polygone correspondant trouvé pour l'ID {id} dans le deuxième fichier.")
 
@@ -167,5 +162,5 @@ def compute_iou(csv1, csv2):
     
 
 
-compute_iou("utp-0110-014v.gif.csv", "test.csv")
+#compute_iou("utp-0110-014v.gif.csv", "test.csv")
 
